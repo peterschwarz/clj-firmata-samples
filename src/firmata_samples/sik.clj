9b461a7f5f27e3433dbc607cd3aecc49b09dbdf2
Peter Schwarz <peter.schwarz@bicycle.io>
2014-04-12 12:01:47 -0500

    Added SIK example 3: RGB LED

--- src/firmata_samples/sik.clj
+++ src/firmata_samples/sik.clj
@@ -5,6 +5,10 @@
             [firmata.receiver :refer :all]
             [clojure.core.async :refer [timeout <!! <!]]))
 
+(defn- wait-to-settle []
+  ; TODO: This can be removed in future releases of clj-firmata
+  (println "waiting for things to settle")
+  (<!! (timeout 2000)))
 
 (defexample blink
   "1) Blinking an LED
@@ -29,9 +33,7 @@
   Read the values off of a potentiometer wired to analog pin A0"
   [board (open-board port-name)]
 
-  ; TODO: This can be removed in future releases of clj-firmata
-  (println "waiting for things to settle")
-  (<!! (timeout 2000))
+  (wait-to-settle)
 
   (let [sensor (atom 1000)]
     (println "Enabling analog-in reporting")
@@ -50,5 +52,98 @@
 
        (set-digital board pin :low)
 
-       (<! (timeout sleep-time))))
-  ))
+       (<! (timeout sleep-time))))))
+
+(declare main-colors show-spectrum)
+
+(defexample rgb-led
+  "3) RGB LED
+
+  Make an RGB LED display a rainbow of colors!"
+  [board (open-board port-name)]
+
+  (wait-to-settle)
+
+  (let [red-pin 9
+        green-pin 10
+        blue-pin 11]
+    (run-loop
+
+     (main-colors board red-pin green-pin blue-pin)
+
+     (show-spectrum board red-pin green-pin blue-pin))))
+
+(defn- main-colors
+  "Displays the main RGB colors, one at a time"
+  [board red-pin green-pin blue-pin]
+
+  (set-pin-mode board red-pin :output)
+  (set-pin-mode board green-pin :output)
+  (set-pin-mode board blue-pin :output)
+
+
+  (let [sleep-time 1000]
+    (<!! (timeout sleep-time))
+
+    (set-digital board red-pin :high)
+    (set-digital board green-pin :low)
+    (set-digital board blue-pin :low)
+
+    (<!! (timeout sleep-time))
+
+    (set-digital board red-pin :low)
+    (set-digital board green-pin :high)
+    (set-digital board blue-pin :low)
+
+    (<!! (timeout sleep-time))
+
+    (set-digital board red-pin :low)
+    (set-digital board green-pin :high)
+    (set-digital board blue-pin :high)
+
+    ; cyan
+    (<!! (timeout sleep-time))
+
+    (set-digital board red-pin :low)
+    (set-digital board green-pin :high)
+    (set-digital board blue-pin :low)
+
+    ; purple
+    (<!! (timeout sleep-time))
+
+    (set-digital board red-pin :high)
+    (set-digital board green-pin :low)
+    (set-digital board blue-pin :high)
+
+    ; white
+    (<!! (timeout sleep-time))
+
+    (set-digital board red-pin :high)
+    (set-digital board green-pin :high)
+    (set-digital board blue-pin :high)
+
+    (<!! (timeout sleep-time))))
+
+(defn- show-spectrum
+  [board red-pin green-pin blue-pin]
+  (set-pin-mode board red-pin :pwm)
+  (set-pin-mode board green-pin :pwm)
+  (set-pin-mode board blue-pin :pwm)
+  (doseq [x (range 768)]
+    (cond
+      (<= x 255) (do
+                   (set-analog board red-pin (- 255 x))
+                   (set-analog board green-pin x)
+                   (set-analog board blue-pin 0))
+      (<= x 511) (do
+                   (set-analog board red-pin 0)
+                   (set-analog board green-pin (- 255 x 256))
+                   (set-analog board blue-pin (- x 256)))
+      :else (do
+                   (set-analog board red-pin (- x 512))
+                   (set-analog board green-pin 0)
+                   (set-analog board blue-pin (- 255 x 512))))
+
+    (<!! (timeout 10))
+    )
+  )
