c0deaa5053299a1c0ada5b8905d8d4d96b56b6aa
Peter Schwarz <peter.schwarz@bicycle.io>
2014-06-13 17:26:41 -0500

    project desc and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-firmata-samples "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Examples of the clj-firmata library (https://github.com/peterschwarz/clj-firmata)"
+  :url "https://github.com/peterschwarz/clj-firmata-samples"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
