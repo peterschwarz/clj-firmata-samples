ba6c79cedbcb694b455d6f13aeae2e407648813d
Peter Schwarz <peter.schwarz@bicycle.io>
2014-07-09 14:11:45 -0500

    updated to latest clj-firmata

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-firmata "1.1.0"]])
+                 [clj-firmata "1.1.1"]])
